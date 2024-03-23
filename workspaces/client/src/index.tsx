3792c51559712969750f68f33dc430a3975cf8bc
komura-c <37304826+komura-c@users.noreply.github.com>
2024-03-24 00:43:03 +0900

    fix: admin lazy

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -4,7 +4,6 @@ import { createRoot, hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
 
-import { AdminApp } from '@wsh-2024/admin/src/index';
 import { ClientApp } from '@wsh-2024/app/src/index';
 
 // import { preloadImages } from './utils/preloadImages';
@@ -16,6 +15,7 @@ const main = async () => {
 
   const root = document.getElementById('root')!;
   if (window.location.pathname.startsWith('/admin')) {
+    const { AdminApp } = await import('@wsh-2024/admin/src/index');
     createRoot(root).render(<AdminApp />);
   } else {
     hydrateRoot(
