d91aafcfb41fb8905082403150eb3d70b9251669
komura-c <37304826+komura-c@users.noreply.github.com>
2024-03-24 09:55:26 +0900

    fix: get image jpg

--- workspaces/admin/src/lib/image/isSupportedImage.ts
+++ workspaces/admin/src/lib/image/isSupportedImage.ts
@@ -2,7 +2,7 @@ import { fileTypeFromBuffer } from 'file-type';
 import { Magika } from 'magika';
 
 const SUPPORTED_MAGIKA_LABEL_LIST = ['bmp', 'jpeg', 'png', 'webp'];
-const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif', 'image/jxl'];
+const SUPPORTED_MIME_TYPE_LIST = ['image/bmp', 'image/jpeg', 'image/png', 'image/webp', 'image/avif'];
 
 const magika = new Magika();
 
