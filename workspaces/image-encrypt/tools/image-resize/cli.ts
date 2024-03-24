89dfbbd0e2323e6c58f48a3e0cea2c59b4d47864
komura-c <37304826+komura-c@users.noreply.github.com>
2024-03-24 15:56:36 +0900

    fix: re compress image

--- workspaces/image-encrypt/tools/image-resize/cli.ts
+++ workspaces/image-encrypt/tools/image-resize/cli.ts
@@ -2,16 +2,16 @@ import fg from 'fast-glob';
 import sharp from 'sharp';
 
 const folder = './tools/image-resize/';
-const inputFilePath = folder + 'input/**.jpg';
+const inputFilePath = folder + 'input/**.png';
 const outputFolderPath = 'output/';
 
 fg(inputFilePath).then((strings: string[])=>{
   strings.map(async (path: string) => {
     console.log(path)
-    const outputFilePath = path.replace("input/", outputFolderPath).replace(".jpg", ".webp")
+    const outputFilePath = path.replace("input/", outputFolderPath).replace(".png", ".webp")
     console.log(outputFilePath)
     await sharp(path)
-      .resize(256, 256)
+      .resize(538, 760)
       .webp()
       .toFile(outputFilePath);
   })
